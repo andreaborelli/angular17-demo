b3d15d56282290fefcdae27f45d696cec41f616c
Andrea Borelli <andreaborelli@blu.it>
2024-09-10 12:46:37 +0200

    Interpolation - Template Tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,31 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  <h1>{{yourName}}</h1>
+  {{currentIndex}} <!-- Interpolation - Template Tags -->`,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular17-demo';
+
+  yourName = 'Andrea';
+
+  currentIndex = 10;
+
+  constructor() {
+    setInterval(() =>{
+      this.currentIndex++;
+    }, 2000)
+  }
 }
+
+
+/* template renderizzato > aggiornato in automatico
+dietro le quinte c'è un meccanismo chiamato change detection
+in pratica il template di un componente angular
+viene automaticamente renderizzato
+ogni volta che ci sono 3 situazioni/scenari
+   1 event di mouse e tastiera
+   2 operazioni lato server, che appunto comunicano con una REST API
+   3 timer: setInterval, setTimeout */
