02757ce7b9ba062125d199fcfcd9111e3745cf5c
Andrea Borelli <andreaborelli@blu.it>
2024-09-10 15:17:18 +0200

    Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,17 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [],
   template: `
-  <h1>{{yourName}}</h1>
-  {{currentIndex}} <!-- Interpolation - Template Tags -->`,
+  <button (click)="currentIndex = currentIndex + 1">+</button> <!-- Events -->
+  <span>{{currentIndex}}</span>
+  <button (click)="currentIndex = currentIndex - 1">-</button>
+  `,
   styles: [],
 })
 export class AppComponent {
 
-  yourName = 'Andrea';
 
-  currentIndex = 10;
+  currentIndex = 0;
 
-  constructor() {
-    setInterval(() =>{
-      this.currentIndex++;
-    }, 2000)
-  }
 }
 
 
-/* template renderizzato > aggiornato in automatico
-dietro le quinte c'è un meccanismo chiamato change detection
-in pratica il template di un componente angular
-viene automaticamente renderizzato
-ogni volta che ci sono 3 situazioni/scenari
-   1 event di mouse e tastiera
-   2 operazioni lato server, che appunto comunicano con una REST API
-   3 timer: setInterval, setTimeout */
