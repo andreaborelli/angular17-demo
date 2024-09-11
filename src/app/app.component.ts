fb1ee99aba7b0cc07517493474fdb21d4de688d3
Andrea Borelli <andreaborelli@blu.it>
2024-09-11 15:46:54 +0200

    CSS Esterni

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,23 @@ import { RouterOutlet } from '@angular/router';
   imports: [],
   template: `
 
-    <button (click)="dec()">-</button>
-    <button (click)="inc()">+</button>
+<div class="alert primary">
+        message
+    </div>
 
-<hr>
-    <a [href]="website">visit website</a> <!-- text interpolation -->
-    <!-- per vedere il contenuto della proprietà di website dobbiamo utilizzare
-     le parentesi quadre attorno all'attributo href [href] questa tecnica si chiama Property binding
-     perchè mettiamo in sincronia una proprietà con un attributo, è quando la proprietà viene aggiornata
-     anche l'attributo automaticamente si aggiornerà. Le parentesi quadre indicano che come valore
-     un attributo non avrà più una stringa, ma un espressione come fossero delle parentesi graffe,
-     questo meccanismo si puù utilizzare per qualunque elemento. -->
-  <br>
-     <img [src]="image" [alt]="description" [width]="width">
-     <!-- qualunque attributo può essere dinamico -->
+    <div class="alert danger">
+        message
+    </div>
+
+    <div class="alert success">
+        message
+    </div>
 
   `,
-  styles: [],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
 
-  website = 'htttps://www.fabiobiondi.dev';
-
-  image = 'https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/2023/2023-11/angular-logo-1200-628.png?sfvrsn=bf64b1ee_3'
-
-  width = 100;
-
-  description = 'Angular logo';
-
-  dec() {
-    if (this.width > 100)
-    this.width = this.width - 10;
-  }
-  inc() {
-    this.width = this.width + 10;
-  }
 
 
 }
