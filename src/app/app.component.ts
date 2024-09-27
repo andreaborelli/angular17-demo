432430f0bd1a4e7d71a9ce15ad3807cb1d098a0e
Andrea Borelli <andreaborelli@blu.it>
2024-09-27 18:29:23 +0200

    Bootstrap CSS via CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,99 +1,41 @@
 import { Component } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    NgStyle
+    NgClass
   ],
   template: `
-
-<!-- dynamic class -->
-  <div
-    class="alert"
-    [ngStyle]="styles"
-
-    >
-        {{text}}
-
-  <!-- con la direttiva ngStyle applichiamo un oggetto di proprietà CSS direttamente ad un elemento del template,
-   richiede anche di essere importata dal pacchetto angular common: import { NgStyle } from '@angular/common';
- e inserita tra gli import del componente affinchè possa essere utlilizzata -->
-
-<!-- inline-style classi CSS in in line in modo dinamico, flessibile -->
-
-  </div>
-
-  <!-- styles è il valore della direttiva ngStyle -->
-
-  <button (click)="styles = { backgroundColor: 'cyan', color: 'black',fontSize: '10px' }">Primary</button>
-  <button (click)="styles = { backgroundColor: 'red', color: 'white', fontSize: '20px' }">Danger</button>
-  <button (click)="styles = { backgroundColor: 'green', color: 'white', fontSize: '30px' }">Success</button>
-
-<!-- una soluzione alternativa che invoca una funzione:
-
- [ngStyle]="getStyles()"
-
-invece di impostare una proprietà:
-
- <div
+    <!-- classe alert hardcoded quindi statica, infatti la possiamo ispezionere nell'HTML -->
+    <div
       class="alert"
-      [ngStyle]="getStyles()"
+      [ngClass]="{
+        'alert-primary': alert.type === 'primary',
+        'alert-danger': alert.type === 'danger',
+        'alert-success': alert.type === 'success',
+      }"
     >
-        {{ alert.msg }}
+    {{ alert.msg }} - {{ alert.type }}
     </div>
 
     <button (click)="alert = {msg: 'hello 1', type: 'primary'}">primary</button>
     <button (click)="alert = {msg: 'hello 2', type: 'danger'}">danger</button>
     <button (click)="alert = {msg: 'hello 3', type: 'success'}">success</button>
-
- -->
+      <!-- https://getbootstrap.com/ Include via CDN CSS di bootstrap servito da un host esterno -->
 
   `,
 
   styles: `  /* la regola css non viene inserita in un array con parentesi quadre */
-    .alert {
-      padding: 20px;
-      border: 4px solid black;
-      border-radius: 20px;
-  }
+
   `,
 })
 export class AppComponent {
 
-  // alert = 'success';
-  // msg = 'hello alert!';
-
-  // oggetto alert
-  styles = {
-    backgroundColor: 'red',
-    color: 'white',
-    fontSize: '100px'
-  }
-  text = "Hello CSS";
-
-
-
- /* una soluzione alternativa che invoca una funzione:
-
-  [ngStyle]="getStyles()"
-
- invece di impostare una proprietà:
-
-   alert: Alert = {
+  alert = {
     msg: 'hello',
-    type: 'primary'
+    type: 'primary' // tipo di classe css che voglio applicare
   }
-
-  getStyles() {
-    switch (this.alert.type) {
-      case 'danger': return { backgroundColor: 'darkred', text: 'white' }
-      case 'success': return { backgroundColor: 'lightgreen', text: 'black' }
-      default:
-        return  { backgroundColor: 'blueviolet', text: 'white' }
-    }
-  }
- */
 
 }
