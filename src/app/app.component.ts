2afe31262ee577f657c5aef1b94036dd50113355
Andrea Borelli <andreaborelli@blu.it>
2024-10-15 17:17:13 +0200

    Pipes + info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,15 @@ type Alert = {
   ],
   template: `
 
-        <!-- Pipes | : buit-in sono dei formatter di dati  -->
+        <!-- Pipes | : buit-in sono dei formatter di dati,
+          realtà un formatter è un array di un certo tipo filtrato in un array di un'altro tipo,
+          con meo o più elementi, rimappato, o un array di oggetti che diventi un array di stringhe, indifferente  -->
+
+           <!-- il vantaggio delle pipe non è solo il formatter di per se, è che hanno performance molto elevate
+            perchè la funzione del pipe viene invocata solo quando cambia il valore che gli passiamo
+            è non selvaggiamente, come invece potrebbe accadere se chiamiamo una funzione
+            nel template che restituisce il valore formattato, che invece verrebbe invocato
+            ogni volta che il template viene rirenderizzato   -->
 
   <div class="centered-page sm flex flex-col gap-3">
 
@@ -29,9 +37,11 @@ type Alert = {
                                                               4 numero massimo di cifre -->
 
 <pre>{{user | json}}</pre> <!-- json: formatta l'oggetto in json; una sorta di JSON stringify -->
-<pre>{{users | json}}</pre>
+<pre>{{users | json}}</pre> <!-- async pipe permette di sottoscrivere un observable direttamente nel template -->
+<!-- custom pipe: creare un pipe custom per formattare i dati in modo personalizzato -->
 
   </div>
+
 
   `,
 
