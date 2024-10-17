fe1e00aa12947ae5a48901f7b9b6e45aacbd65cd
Andrea Borelli <andreaborelli@blu.it>
2024-10-17 17:51:01 +0200

    ngSwitch, union type e strict mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ type Product = {
   cost: number;
 }
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,56 +17,34 @@ type Product = {
   ],
   template: `
 
+
     <!-- come renderizzare una lista di elementi acquisiti da un array es. di prodotti -->
 
     <div class="centered-page sm">
-      <li *ngFor="let product of products; let i = index; let last = last; let odd = odd"
-      [style.border-bottom]="last ? '2px solid white' : null"
-      [style.background-color]="odd ? 'gray': 'cyan'">
-        {{i + 1}} - {{product.name}} {{last}}<!-- product elemento su cui stiamo iterando -->
-      </li>
-      <pre>{{ products | json }}</pre>
 
-      <!-- Il punto interrogativo (?) in Angular è usato per l'operatore ternario,
-      che è una forma abbreviata per eseguire una condizione if-else all'interno del template.
+    <button class="btn" (click)="section = 'home'">Home</button>
+    <button class="btn" (click)="section = 'step1'">Step 1</button>
+    <button class="btn" (click)="section = 'step2'">Step 2</button>
+    <button class="btn" (click)="section = 'final'">Final</button>
+    <hr>
+  <div [ngSwitch]="section"> <!-- ngSwitch è una direttiva che permette di fare un controllo su una variabile
+                                  e in base al suo valore renderizza un blocco di codice o un altro -->
+    <div *ngSwitchCase = "'home'">Home</div> <!-- *ngSwitchCase che accetta un'espressione,
+                                                  per questo passiamo la stringa tra apicetti,
+                                                  potrebbe essere anche una variabile -->
+    <div *ngSwitchCase = "'step1'">Step 1</div>
+    <div *ngSwitchCase = "'step2'">Step 2</div>
+    <div *ngSwitchCase = "'final'">Final</div>
+    <div *ngSwitchDefault>Select a button</div> <!-- *ngSwitchDefault è un blocco di codice che viene renderizzato
+                              quando nessun *ngSwitchCase è soddisfatto -->
+      <!-- <div *ngIf="section === 'home'">Home</div>
+      <div *ngIf="section === 'step1'">Step 1</div>
+      <div *ngIf="section === 'step2'">Step 2</div>
+      <div *ngIf="section === 'final'">Final</div> -->
 
-      condizione ? valore_se_vero : valore_se_falso
+          <!-- tanti if che fanno un contollo sulla stessa variabile e bene usare ngSwitch -->
 
-      condizione: Se è vera, Angular applica il valore_se_vero.
-        Altrimenti, applica il valore_se_falso.
-      -->
-
-      <!-- [style.border-bottom]="last ? '2px solid white' : null" se è l'ultimo elemento (last) applica il bordo altrimenti null, non facciamo nulla
-
-      <!-- products è l'intero array, che contiene più elementi (nel tuo caso un array di oggetti Product).
-            product è la variabile singola che rappresenta il singolo elemento su cui si sta iterando
-            durante ciascun ciclo del *ngFor. -->
-
-      <!-- Significa che per ogni ciclo *ngFor, Angular estrae un singolo product dall'array products,
-       e quindi puoi accedere alle proprietà del singolo product (come product.id, product.name, ecc.).
-
-        In sintesi, products è l'array su cui stai iterando, mentre product è la singola istanza che
-        rappresenta ogni elemento di quell'array. -->
-
-    <!-- In Angular, all'interno del ciclo *ngFor, oltre a iterare sugli elementi di un array,
-      puoi accedere anche ad alcune informazioni aggiuntive, come l'indice dell'elemento corrente.
-          let i = index
-          La sintassi let i = index è usata per catturare l'indice corrente di ogni iterazione del ciclo *ngFor.
-          L'indice parte da 0 e aumenta di uno per ogni iterazione.
-          Questo può essere utile quando vuoi mostrare la posizione dell'elemento nell'array.
-
-          let product of products: Qui stai iterando su ogni product dell'array products.
-          let i = index: i è la variabile che memorizza l'indice di ogni iterazione.
-          i + 1 array parte da 1 invece che 0 -->
-
-
-          <!-- first: true se l'elemento è il primo dell'array.
-               last: true se l'elemento è l'ultimo dell'array.
-               odd: true se l'indice dell'elemento è dispari.
-               even: true se l'indice dell'elemento è pari.
-               es applicare classi css ad uno specifico elemento
-               -->
-
+  </div>
     </div>
 
 
@@ -77,11 +56,6 @@ type Product = {
 })
 export class AppComponent {
 
-  products: Product[] = [
-    { id: 1, name: 'Chocolate', cost: 3 },
-    { id: 2, name: 'Milk', cost: 1 },
-    { id: 3, name: 'Biscuits', cost: 2 }
-  ]
-
+ section: 'home' | 'step1' | 'step2' | 'final'| null = null;
 
 }
