4ba016fde0e73469683c95d6306c2f280f58e2a8
Andrea Borelli <andreaborelli@blu.it>
2024-10-31 18:28:29 +0100

    @for e computed Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { ListComponent } from './shared/list/list.component';
 import { CommonModule } from '@angular/common';
 
@@ -24,22 +24,27 @@ const initializeState: Product[] = [ // stato iniziale di un array di oggetti
   ],
   template: `
 
-    <!--  @for e @empty blocks-->
-
-    <!-- usando un signal dobbiamo usare product con function con parentesi tonde -->
-
-    <!-- l'espressione track serve per indicare qual'è la chiave univoca che Angular utilizzerà per ottimizzare i render.
-      Andando ad applicare questa chiave univoca, Angular farà si che al render successivo, quando mancherà o verrà aggiunto
-      un nuovo elemento dalla lista, riuscirà a riciclare alcuni elementi del DOM ed evitare
-      che vengano effettuate delle operazioni sugli elementi che già esistevano nel render precedente e che sono identici
-      nel render succcessivo.
-      É un ottimizzazione che anche nel ngFor si poteva, e anzi si doveva fare, se volevamo tenere il massimo delle performance
-      da una lista dinamica. Tuttavia con @for è diventata obbligatoria.
-      Utilizziamo product.id che di solito è la proprietà univoca che differenzia ogni elemento della lista in modo sicuro. -->
+    <!--  @for e computed Signals -->
 
     <!-- il @for ci permette anche di utilizzare un nuovo comando in associazione, che è @empty,
      che in pratica al suo interno potrà renderizzare degli elementi del DOM, quindi del template,
      nel momento in cui l'array è vuoto, oppure appunto non contiene valori come NULL o UNDEFINED.  -->
+
+     <!-- Tuttavia non sempre vogliamo renderizzare il contenuto nello stesso punto in cui c'è la lista.
+      Potremmo avere una view molto complessa e avere dell'altro contenuto per es.
+      altrove che vogliamo nascondere o visualizzare quando ci sono o non ci sono elementi nella lista.
+      In tal caso possiamo creare una computed property es. uno stato derivato da products che chiameremo
+      es. noItems, indicando che non ci sono elementi nel carrello o comunque visibili e
+      semplicemente restituirà un booleano quando la length di products è zero:
+      noItems = computed(() => this.products().length === 0);
+      poi utiliziamo ngIf per renderizzare il messaggio ci son N prodotti quando ci sono elementi per questo usiamo
+      il not ! davanti a noItems, è all'interno possiamo visualizzare anche il contenuto:
+      <div *ngIf="!noItems()">There are {{products().length}} products</div>
+      solo quando i prodotti sono nel carrello, altrimenti è inutile visualizzare il messaggio
+      allo stesso modo possiamo sostituire anche il product length con un signal derivato che ci restituisce,
+      in questo caso semplicemente la length e non un booleano.
+      abbiamo visto come Mixare direttive con i nuovi blocchi for if ecc.
+      -->
 
     <div class="centered-page sm">
 
@@ -49,6 +54,8 @@ const initializeState: Product[] = [ // stato iniziale di un array di oggetti
           <div>Empty list</div>
           <button class="btn" (click)="loadProducts()">Load</button>
       }
+
+      <div *ngIf="!noItems()">There are {{ totalProducts() }} products</div>
 
       <!-- <pre>{{ products() | json}}</pre> -->
 
@@ -63,6 +70,10 @@ const initializeState: Product[] = [ // stato iniziale di un array di oggetti
 export class AppComponent {
 
  products = signal<Product[]>([]);
+
+ noItems = computed(() => this.products().length === 0);
+
+ totalProducts = computed(() => this.products().length);
 
  loadProducts() {
     this.products.set(initializeState);
