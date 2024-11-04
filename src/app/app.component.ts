c71ef903a6384b406ecde64b11c5d6452f3b6323
Andrea Borelli <andreaborelli@blu.it>
2024-11-04 12:51:16 +0100

    Contextual Variables, @If inside @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,11 @@ type Product = {
   cost: number;
 }
 
-const initializeState: Product[] = [ // stato iniziale di un array di oggetti
-  { id: 1, name: 'Chocolate', cost: 3 },
-  { id: 2, name: 'Milk', cost: 1 },
-  { id: 3, name: 'Biscuits' , cost: 2 }
+const initialState: Product[] = [
+  {id: 1, name: 'Chocolate', cost: 3},
+  {id: 2, name: 'Milk', cost: 1},
+  {id: 3, name: 'Biscuits', cost: 2},
 ]
-
 
 @Component({
   selector: 'app-root',
@@ -24,41 +23,26 @@ const initializeState: Product[] = [ // stato iniziale di un array di oggetti
   ],
   template: `
 
-    <!--  @for e computed Signals -->
+    <!-- Contextual Variables, @If inside @for -->
 
-    <!-- il @for ci permette anche di utilizzare un nuovo comando in associazione, che è @empty,
-     che in pratica al suo interno potrà renderizzare degli elementi del DOM, quindi del template,
-     nel momento in cui l'array è vuoto, oppure appunto non contiene valori come NULL o UNDEFINED.  -->
-
-     <!-- Tuttavia non sempre vogliamo renderizzare il contenuto nello stesso punto in cui c'è la lista.
-      Potremmo avere una view molto complessa e avere dell'altro contenuto per es.
-      altrove che vogliamo nascondere o visualizzare quando ci sono o non ci sono elementi nella lista.
-      In tal caso possiamo creare una computed property es. uno stato derivato da products che chiameremo
-      es. noItems, indicando che non ci sono elementi nel carrello o comunque visibili e
-      semplicemente restituirà un booleano quando la length di products è zero:
-      noItems = computed(() => this.products().length === 0);
-      poi utiliziamo ngIf per renderizzare il messaggio ci son N prodotti quando ci sono elementi per questo usiamo
-      il not ! davanti a noItems, è all'interno possiamo visualizzare anche il contenuto:
-      <div *ngIf="!noItems()">There are {{products().length}} products</div>
-      solo quando i prodotti sono nel carrello, altrimenti è inutile visualizzare il messaggio
-      allo stesso modo possiamo sostituire anche il product length con un signal derivato che ci restituisce,
-      in questo caso semplicemente la length e non un booleano.
-      abbiamo visto come Mixare direttive con i nuovi blocchi for if ecc.
-      -->
-
-      <!-- VERSIONE: @if block -->
+      <!-- il comando FOR supporta anche delle variabili contestuali,
+       è possibile recuperare l'index es. del prodotto cui stiamo iterando
+       è salvarlo in una variabile i da mostrare poi all'interno del FOR su ogni elemento -->
 
     <div class="centered-page sm">
 
-    @for (item of products(); track item.id) {
-          <li>{{item.name}}</li>
-      } @empty {
-          <button (click)="load()">Load</button>
-      }
-      @if(!noItems()) {
-          <h1>Total Items: {{products().length}}</h1>
-          <h1>Total Items: {{totalItems()}}</h1>
-      }
+    @for (product of products(); track product.id; let i = $index; let last = $last) {
+              <li>
+                  {{i + 1}}. {{product.name}}
+                  @if(last) {
+                    <hr>
+                  }
+              </li>
+          } @empty {
+              <button class="btn" (click)="loadProducts()">Load</button>
+          }
+
+          <div *ngIf="!noItems()">There are {{ totalProducts() }} products</div>
       <!-- <pre>{{ products() | json}}</pre> -->
 
     </div>
@@ -71,17 +55,13 @@ const initializeState: Product[] = [ // stato iniziale di un array di oggetti
 })
 export class AppComponent {
 
-  products = signal<Product[]>([]);
+  products = signal<Product[]>([])
 
   noItems = computed(() => this.products().length === 0)
-  totalItems = computed(() => this.products().length)
+  totalProducts = computed(() => this.products().length)
 
-  load() {
-    this.products.set([
-      {id: 1, name: 'Chocolate', cost: 3},
-      {id: 2, name: 'Milk', cost: 1},
-      {id: 3, name: 'Biscuits', cost: 2},
-    ])
+  loadProducts() {
+    this.products.set(initialState)
   }
 }
 
