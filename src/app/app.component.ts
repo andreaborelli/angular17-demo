8ae3b7c0f3d4c192cd1e7d3299743b5467548f38
Andrea Borelli <andreaborelli@blu.it>
2024-11-04 18:28:30 +0100

    Multiple SIgnals - create a tabbar navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,6 @@ import { Component, computed, signal } from '@angular/core';
 import { ListComponent } from './shared/list/list.component';
 import { CommonModule } from '@angular/common';
 
-type Product = {
-  id: number;
-  name: string;
-  cost: number;
-}
-
-const initialState: Product[] = [
-  {id: 1, name: 'Chocolate', cost: 3},
-  {id: 2, name: 'Milk', cost: 1},
-  {id: 3, name: 'Biscuits', cost: 2},
-]
 
 @Component({
   selector: 'app-root',
@@ -23,35 +12,67 @@ const initialState: Product[] = [
   ],
   template: `
 
-    <!-- Contextual Variables, @If inside @for -->
+  <!-- Multiple SIgnals - create a tabbar navigation -->
 
-      <!-- il comando FOR supporta anche delle variabili contestuali,
-       è possibile recuperare l'index es. del prodotto cui stiamo iterando
-       è salvarlo in una variabile i da mostrare poi all'interno del FOR su ogni elemento -->
+  <!-- al click del pulsante mostra il contenuto dell'oggetto -->
 
-       <!--  VERSIONE AGGIORNATA.
-          Utilizziamo le contextual properties direttamente all'interno del blocco senza
-          la necessità di usare degli alias -->
+  <!-- es. che usa sia il FOR che l'IF all'interno dello stesso template
+   abbiamo unarray di prodotti e utilizziamo il FOR per visualizzare n pulsanti
+   sulla base del numero di prodotti, e ora immaginiamo di voler la descrizione per ogni prodotto
+   che aggiungiamo anche al type. Al click del pulsante vogliamo visualizzare la descrizione
+   sotto a tale pulsanti.
+   Inanzitutto potermmo fare: al click di un pulsante andare ad invocare il metodo selectProduct()
+   passando il prodotto che ho cliccato: selectProduct(product)
 
-          <!-- first: primo elemeto
-               odd: elemento dispari
-               even: elemento pari
-                -->
+   selectProduct(product: Product){
+
+  }
+   Questo prodotto lo salviamo in un nuovo signal che indica qual'è il prodotto attivo,
+   il prodotto selezionato, che chiamiamo:
+
+   activeProduct = signal<Product | null>(null);
+
+   che è un signal che inizializiamo a null, visto che ovviamente all'inizio, all'avvio del componente
+
+      activeProduct = signal<Product | null>(null); non sarà valorizzato ma potrà contenere NULL,
+        come viene specificato tra i generics (parentesi angolari) oppure un prodotto usando union type di typeScript |
+
+        quindi nel metodo di selectProduct() possiamo scrivere:
+
+        this.activeProduct.set(product);
+
+        con il metodo set() dei signal e salvare al suo interno il nostro prodotto
+
+        Così facendo ad ogni click activeProduct verrà valorizzato.
+        infatti con PIPE JSON è vedremo che al click abbiamo l'intero oggetto del prodotto selezionato,
+        è potremmo visualizzare la descrizione, il name, il costo ecc.
+
+        forse: dobbiamo usare il punto di domanda ? perchè activeProduct come specificato:
+
+        <Product | null> è potenzialmente NULL e quindi se non avessimo lo STRICT MODE () di TypeScript
+        in questo caso avremmo un eccezione al primo render perchè essendo NULL
+        il nostro oggetto di activeProduct
+
+              <pre>{{ activeProduct()?.description}}</pre>
+
+        description ovviamente sarebbe letto da un oggetto null generando un eccezzione a runtime
+
+        quindi usiamo il ? per evitare questo problema. ora al click vedremo la descrizione del prodotto,
+        oppure il costo, il nome ecc.
+      -->
 
     <div class="centered-page sm">
     @for (product of products(); track product.id) {
-              <li>
-                  {{$index + 1}}. {{product.name}}
-                  @if($last) {
-                      <hr>
-                  }
-              </li>
-          } @empty {
-              <button class="btn" (click)="loadProducts()">Load</button>
-          }
 
-          <div *ngIf="!noItems()">There are {{ totalProducts() }} products</div>
-      <!-- <pre>{{ products() | json}}</pre> -->
+      <button class="btn" (click)="selectProduct(product)">{{ product.name }}</button>
+                  }
+      @if (activeProduct()) {
+        <pre>{{ activeProduct()?.name}}</pre>
+        <pre>{{ activeProduct()?.cost}}</pre>
+        <pre>{{ activeProduct()?.description}}</pre>
+      } @else {
+        <div>Select a product</div>
+      }
 
     </div>
 
@@ -63,13 +84,26 @@ const initialState: Product[] = [
 })
 export class AppComponent {
 
-  products = signal<Product[]>([])
 
-  noItems = computed(() => this.products().length === 0)
-  totalProducts = computed(() => this.products().length)
+  products = signal<Product[]>([
+    { id: 1, name: 'Chocolate', cost: 3, description: 'lorem...' },
+    { id: 2, name: 'Milk', cost: 1, description: 'bla bla..' },
+    { id: 3, name: 'Biscuits', cost: 2, description: 'super good!' },
+  ]);
 
-  loadProducts() {
-    this.products.set(initialState)
+  activeProduct = signal<Product | null>(null);
+
+  selectProduct(product: Product){
+    this.activeProduct.set(product);
+    console.log(product);
   }
+
+}
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  cost: number;
 }
 
