f28144ffb5a6433fa8b31f4eb7d3f7b9ee37cde2
Andrea Borelli <andreaborelli@blu.it>
2024-11-27 19:07:42 +0100

    Alert - Customize Buttons - ngClass

--- src/app/shared/components/alert.component.ts
+++ src/app/shared/components/alert.component.ts
@@ -36,9 +36,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       <div><ng-content></ng-content></div>
 
       <div>
-        <button class="btn btn-sm" (click)="onCancel.emit()">{{ denyLabel }}</button>
-        <button class="btn btn-sm btn-primary" (click)="onConfirm.emit()">{{ acceptLabel }}</button>
+
+        <button class="btn btn-sm"
+            (click)="onCancel.emit()"
+            >{{ denyLabel }}
+            </button>
+
+        <button class="btn btn-sm"
+        [ngClass]="{
+                'btn-info': variant === 'info',
+                'btn-success': variant === 'success',
+                'btn-error': variant === 'error'
+              }"
+          (click)="onConfirm.emit()"
+          >{{ acceptLabel }}
+        </button>
+
       </div>
+      
     </div>
 
   `,
