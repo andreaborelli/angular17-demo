8a3b61f925a6bf1d4babac4a5d7e14f1bb03d593
Andrea Borelli <andreaborelli@blu.it>
2024-11-28 18:04:14 +0100

    Alert - Icon - Children Components

--- src/app/shared/components/alert.component.ts
+++ src/app/shared/components/alert.component.ts
@@ -1,12 +1,14 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { VariantIconComponent } from "./variant-icon.component";
 
 @Component({
   selector: 'app-alert',
   standalone: true,
   imports: [
-    NgClass
-  ],
+    NgClass,
+    VariantIconComponent
+],
   template: `
 
     <div role="alert"
@@ -18,20 +20,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
           }"
          >
 
-        @switch (variant) {
-          @case ('info') {
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-          }
-          @case('success') {
-              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          }
-          @case('error') {
-              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          }
-          @default {
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-          }
-        }
+      <app-variant-icon [variant]="variant"/>
 
       <div><ng-content></ng-content></div>
 
@@ -53,7 +42,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
         </button>
 
       </div>
-      
+
     </div>
 
   `,
---
+++ src/app/shared/components/variant-icon.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-variant-icon',
+  standalone: true,
+  imports: [],
+  template: `
+        @switch (variant) {
+          @case ('info') {
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          }
+          @case('success') {
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          }
+          @case('error') {
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          }
+          @default {
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          }
+        }
+  `,
+  styles: ``
+})
+export class VariantIconComponent {
+
+  @Input() variant: 'info' | 'success' | 'error' | undefined; // union literal types
+
+
+}
