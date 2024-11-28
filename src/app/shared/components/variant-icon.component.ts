e340f4854a2abee415743775c7b8b6aa3053a0cb
Andrea Borelli <andreaborelli@blu.it>
2024-11-28 18:34:54 +0100

    Alert - Dynamic SVG Path con [attr.prop] - property vs attribute

--- src/app/shared/components/variant-icon.component.ts
+++ src/app/shared/components/variant-icon.component.ts
@@ -5,20 +5,31 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-        @switch (variant) {
+        <!-- @switch (variant) {
           @case ('info') {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
           }
           @case('success') {
-              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="" /></svg>
           }
           @case('error') {
               <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
           }
           @default {
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
           }
-        }
+        } -->
+
+        <svg
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        class="stroke-current shrink-0 w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+
+          [attr.d]="icons[variant || 'info']"> <!-- attr perchè d è un attributo, che non ha una proprietà del DOM -->
+
+        </path>
+      </svg>
+
   `,
   styles: ``
 })
@@ -26,5 +37,10 @@ export class VariantIconComponent {
 
   @Input() variant: 'info' | 'success' | 'error' | undefined; // union literal types
 
+  icons = {
+    info: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+    success: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+    error: 'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+  }
 
 }
