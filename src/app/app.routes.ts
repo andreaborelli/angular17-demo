c5623b3be869886c6ed19f13695fa6d8f4a12aa0
Andrea Borelli <andreaborelli@blu.it>
2024-12-10 18:34:23 +0100

    Functional Router Guard - auth.guard.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'demo1', loadComponent: () => import('./features/demo1/demo1.component'),
     data: { title: 'Hello Demo 1'}
   },
-  { path: 'demo2', loadComponent: () => import('./features/demo2/demo2.component') },
+  { path: 'demo2', loadComponent: () => import('./features/demo2/demo2.component'),},
   { path: 'demo3', loadComponent: () => import('./features/demo3/demo3.component') },
 
   {
     path: 'uikit',
+    canActivate: [authGuard],
     loadComponent: () => import('./features/uikit/uikit.component'),
-
     children: [
       { path: 'accordion', loadComponent: () => import('./features/uikit/pages/accordion-demo.component') },
       { path: 'alert', loadComponent: () => import('./features/uikit/pages/alert-demo.component') },
@@ -26,6 +28,78 @@ export const routes: Routes = [
   { path: '', redirectTo: 'demo1', pathMatch: 'full' }
 ];
 
+/*
+  Per proteggere l'accesso ad una root sulla base di una condizione,
+  ovvero accedo in una pagina con un URL, ma NON vogliamo dare permesso all'utente di
+  accedervi se NON sulla base di una condizione che può essere un token che
+  abbiamo salvato, una proprietà nel localStorage, una chiamata al server ecc..
+  bisogna usare il concetto di router guard, una guardia del router.
+  con la proprietà canActivate possiamo definire un array di guardie,
+
+  MENTRE ALCUNE VERSIONI FA DI ANGULAR RICHIEDEVANO UNA ROUTER GUARD SCRITTA CON UNA CLASSE,
+
+  ora possiamo semplicemente definire una funzione al suo interno e questa funzione
+  può restituire per es. true o false, un booleano.
+
+      canActivate: [function() {
+       return false;
+    }],
+
+    se restituiamo false la root no sarà accessibile, infatti es. in demo2 in contenuto non sarà caricato
+    se mettiamo true la root sarà accessibile.
+
+    questa fuozione può essere scritta in arrow sintax,
+
+    canActivate: [() => {
+      return false;
+    }],
+
+    ma il return può dipendere da vari fattori, potremmo fare una chiamata ala server,
+    potremmo avere un valore su localStorage, cookie, oppure recuperare lo stato
+    di authenticazione di sdk, possiamo fare qualunque genere di controllo,
+    potremmo anche iniettare un servizio esterno. potremmo recuperare il valore
+    di login se siamo loggati o meno, o altre informazioni.
+
+    es. simulazione stato di logincon proprietà isLogged che ovviamente dovrà
+    essere recuperata da qualche altra fonte, dal localstorage ai cookie, o sdk ecc..
+    che può essere true o false, quindi es:
+
+         canActivate: [() => {
+      const isLogged = false;
+      return isLogged;
+    }],
+    ma vengo stoppato, quindi la mia view non viene visualizzata, non viene reindirizzato
+    da nessuna parte, semplicemente non mi permette di caricare il contenuto della view.
+
+    quindi all'interno della funzione iniettiamo il router, e se non siamo loggati facciamo
+    un navigateByUrl('demo1') è lo rimandiamo a es. demo1, quindi nel momento in cui accediamo
+    a direttamente a demo2 verremo indeirizzati automaticamente a demo1.
+
+    Comunque è importante che la guardia vada sempre a restituire un booleano o
+    un observable di booleano.
+
+    la guardia si potrebbe anche riutilizzare, quindi megliocreare un file auth.guard.ts
+    è la esportiamo:
+
+    export const authGuard = () => {
+       const router = inject(Router)
+       const isLogged = true;
+        if (!isLogged) {
+        router.navigateByUrl('demo1')
+         }
+         return isLogged
+      }
+
+  canActivate: [AuthGuard], supporta un array di guardie, quindi possiamo avere più guardie
+  una per lo stato di autenticazione, una per i ruoli utente,
+  un'altra che visualizza il contenuto sulla base del fatto, che uno stia pagando un piano
+  es. professional, premium, o free, che è sempre una gestione di ruoli ecc..
+
+  è nel path che definiamo la guardia inseriamo
+
+    canActivate: [AuthGuard],
+
+*/
 
 /*
  creare una root che a sua volta ha dei sottolivelli di root secondarie,
---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,11 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+
+export const authGuard = () => {
+  const router = inject(Router)
+  const isLogged = true;
+  if (!isLogged) {
+    router.navigateByUrl('demo1')
+  }
+  return isLogged
+}
